93785673a568c556f9f3fe88b4deb2b8a007f3cc
PFdev6 <you@example.com>
2018-12-26 02:46:50 +0300

    add confirmation by mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates :nickname, :email, :first_name, :second_name, presence: true   
-  devise :database_authenticatable, :omniauthable, :recoverable, :rememberable, :registerable, :trackable, :validatable
+  devise :confirmable, :database_authenticatable, :omniauthable, :recoverable, :rememberable, :registerable, :trackable, :validatable
   has_many :lots, dependent: :destroy
   has_many :main_news, dependent: :destroy
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,17 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'localhost:3000',
+  user_name:            Rails.application.secrets.mail_user_name,
+  password:             Rails.application.secrets.mail_password,
+  authentication:       'plain',
+  enable_starttls_auto: true }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
