e827c8e3178fc87f6908a2f4d43b203747018412
PFdev6 <you@example.com>
2019-01-07 01:25:52 +0300

    add check price in servicce

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -27,6 +27,7 @@ class LotsController < ApplicationController
 			BroadcastMessage.call(bargain: @lot.current_bargain)
 			redirect_to @lot, success: 'Lot successfully created'
 		else 
+			flash[:error] = t('lot.sp_more_than_ap') if !ComparisonService.check_price?(@lot)
 			flash[:error] = t('lot.change_end_date') if !ComparisonService.check_time?(@lot.lot_end_date)
 			flash[:error] = t('lot.need_img') if !ComparisonService.check_file_count(files)
 			render 'new', danger: 'Lot didn\'t created'
