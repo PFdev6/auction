981c4ba7cc37f293679d022b99254eb3f0dcf823
PFdev6 <fedorovich2012bel@gmail.com>
2019-04-24 23:45:03 +0300

    Revert "Newdev"

---
+++ app/controllers/filter_lots_controller.rb
@@ -0,0 +1,20 @@
+class FilterLotsController < ApplicationController
+  def new
+    @filter_lot = FilterLot.new
+	end
+
+  def show
+    @filter_lot = FilterLot.find(params[:id]).search_lots
+	end
+
+  def create
+    @filter_lot = FilterLot.create(filter_params)
+    redirect_to @filter_lot
+  end
+
+	private 
+  
+  def filter_params
+		params.require(:filter_lot).permit(:user_name, :played_out, :autopurchase_price, :start_price, :new_lots)
+	end 
+end
---
+++ app/models/filter_lot.rb
@@ -0,0 +1,19 @@
+class FilterLot < ApplicationRecord
+  def search_lots(lots)
+    if user_name.present?
+      user_search = User.find_by(['nickname LIKE ?', "%#{user_name}%"]) # where
+      lots = lots.where(user: user_search)
+    end
+    lots = lots.where(["autopurchase_price >= ?", autopurchase_price]) if autopurchase_price.present?
+    lots = lots.where(["start_price >= ?", start_price]) if start_price.present?
+    if played_out.present?
+      lots = lots.select do |lot|
+        lot.current_bargain.present?
+      end
+      lots = lots.select do |lot|
+        lot.current_bargain.played_out == played_out
+      end
+    end
+    lots
+  end 
+end
