f4996661ad209c51da86b8f5fe93f9c702265ecd
Pavel Fedarovich <fedorovich2012bel@gmail.com>
2019-10-03 19:13:13 +0300

    Add user comment spec

--- spec/pages/bargain_page.rb
+++ spec/pages/bargain_page.rb
@@ -1,6 +1,4 @@
 class BargainPage
-  attr_accessor :comments
-
   URLS = { bargain: 'http://localhost:3000/current_bargains/' }.freeze
 
   def initialize(browser, bargain_id)
@@ -11,5 +9,17 @@ class BargainPage
   def do_bid
     @browser.button(id: 'do_bid').click
     @browser
+  end
+
+  def add_comment(message)
+    @browser.textarea(id: 'comment_msg').set(message)
+    @browser.button(id: 'add_comment').click
+  end
+
+  def reply_on_comment(match, message)
+    message_id = @browser.div(text: match).id
+    @browser.link(id: "comment_id_#{message_id}").click
+    @browser.textarea(id: 'comment_msg').set(message)
+    @browser.button(id: 'add_comment').click
   end
 end
